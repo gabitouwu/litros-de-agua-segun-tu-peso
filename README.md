3107c17f67be97292ef8e69afd1b2cddd2f62cfe
gabo <elnogabito@gmail.com>
2022-03-22 14:51:40 -0500

    version 1.1 'litros de agua segun tu peso.'

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-Este programa te permite calcular los litros de agua que debes tomar segun tu peso.
-Tienes que ingresar tu peso seguido de lb si es en libras
-o kg si es en kilos, simple ¿no?
+Ahora tambien te dice cuantos vasos aproximados debes tomar.
--- index.py
+++ index.py
@@ -26,7 +26,8 @@ def comprobarKgOLb(p):
         return lista, 'kg'
     else:
         while True:
-            input('Ingresa tu peso: ')
+            print('¿Cuánto pesas?')
+            p = input()
             
             if p.endswith('b'):
                 p = ' '.join(p)
@@ -56,17 +57,18 @@ while True:
         peso = int(peso)
         peso /= 2.205
         peso *= 35
+        vaso = peso / 250
         peso /= 1000
 
     elif comprobante == 'kg':
         peso = int(peso)
         peso *= 35
+        vaso = peso / 250
         peso /= 1000
-    print()
-    print('Debes consumir (%slitros) al día' % (peso))
 
+    pagina = 'https://www.google.com/search?q=vasos+de+250+ml&source=lmns&bih=667&biw=1264&client=opera&hl=es-419&sa=X&ved=2ahUKEwi8lfPOvdr2AhXGXDABHVTLCmAQ_AUoAHoECAEQAA'
+    
+    print('\nAporximadamente debes consumir "%s litros" de agua al día, o "%s vasos de 250ml" al día.' % (peso, vaso))
+    print("\nVistia la siguiente página para ver como son los vasos.\n%s" % (pagina))
     if not(jugarDeNuevo()):
         break
-
-
-
